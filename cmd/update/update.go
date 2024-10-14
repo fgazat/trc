c57481da12361774137816fe57aaff78b57642fe
Azat Khasanov <aza8048@yandex.ru>
2024-10-14 11:29:26 +0200

    UP

--- cmd/update/update.go
+++ cmd/update/update.go
@@ -7,18 +7,18 @@ import (
 )
 
 func Update() *cobra.Command {
-	var summary string
-	var description string
+	var summary, key, description string
 	cmd := &cobra.Command{
 		Use:              "update",
 		Short:            "Update issue",
 		TraverseChildren: true,
 		Aliases:          []string{"u"},
-		Example:          `  trc u -m "hello" -d "world"`,
+		Example:          `  trc u -k "TEST-1" -m "hello" -d "world"`,
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Println("update issue")
 		},
 	}
+	cmd.Flags().StringVarP(&key, "key", "k", "", "Issue key to update")
 	cmd.Flags().StringVarP(&summary, "sum", "m", "", "Summary. Short -m is due to convinient commit message arg")
 	cmd.Flags().StringVarP(&description, "desc", "d", "", "Description of issue")
 	return cmd
